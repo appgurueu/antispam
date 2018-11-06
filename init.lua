89d059c63006dd7d94da8523f670d2c83c854923
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2018-11-06 20:27:05 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 local PLAYERS_MSG={}
+local PLAYERS_FREQ={}
+local SPAM_SPEED=5
+local SPAM_SPEED_MSECS=SPAM_SPEED*1000
 local SPAM_WARN=5
 local SPAM_KICK=SPAM_WARN+5
 local RESET_TIME=30 --In seconds
@@ -6,10 +9,11 @@ local RESET_TIME_MSECS=RESET_TIME*1000 --Convert to microsecs
 local WARNING_COLOR=minetest.get_color_escape_sequence("#FFBB33")
 minetest.register_on_leaveplayer(function (player)
     PLAYERS_MSG[player:get_player_name()]=nil
+    PLAYERS_FREQ[player:get_player_name()]=nil
 end)
 minetest.register_on_joinplayer(function (player)
 	minetest.after(0,function(player)
-		PLAYERS_MSG[player:get_player_name()]={}
+        PLAYERS_MSG[player:get_player_name()]={}
     end,player)
 end)
 minetest.register_on_chat_message(function(name,message)
@@ -30,5 +34,29 @@ minetest.register_on_chat_message(function(name,message)
     else
         PLAYERS_MSG[name][message]={1,minetest.get_us_time()}
     end
-    return true
+    if PLAYERS_FREQ[name] then
+        local warns=PLAYERS_FREQ[name][4]
+        local amount=PLAYERS_FREQ[name][2]
+        local speed=PLAYERS_FREQ[name][1]
+        local delay=minetest.get_us_time()-PLAYERS_FREQ[name][3]
+        speed=speed*amount+delay/(amount+1)
+        minetest.chat_send_all(speed)
+        if amount >= SPAM_WARN then
+            if warns+1==SPAM_KICK-SPAM_WARN then
+                minetest.kick_player(name,"Kicked for spamming.")
+            elseif speed >= SPAM_SPEED_MSECS then
+                minetest.chat_send_player(name,WARNING_COLOR.."Warning ! You are sending messages too fast. Wait at least "..SPAM_SPEED.." seconds before sending another message.")
+                warns=warns+1
+                speed=SPAM_SPEED
+                amount=SPAM_WARN
+            else
+                speed=0
+                amount=0
+                warns=0
+            end
+        end
+        PLAYERS_FREQ[name]={speed,amount+1,minetest.get_us_time(),warns}
+    else
+        PLAYERS_FREQ[name]={0,0,minetest.get_us_time(),0}
+    end
 end)
